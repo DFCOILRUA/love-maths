bbaeed1be279aef1f629befedc5bd9d2f8978993
DFCOILRUA <danielfolan9@hotmail.com>
2023-01-25 19:04:02 +0000

    Display addition message to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             }  else{
                     let gameType= this.getAttribute("data-type");
-                    alert(`You clicked ${gameType}`);                
+                    runGame(gameType);                
             }
         })
     }
+
+    runGame("addition");
 })
 
 /**
@@ -22,11 +24,19 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's andswer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
 
     // Creates 2 random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAddtionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
 
 function checkAnswer() {
@@ -45,8 +55,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAddtionQuestion() {
-
+function displayAddtionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
